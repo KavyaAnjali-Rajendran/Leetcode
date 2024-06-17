b20bea949648d1c7d2ba603637e68afae4d89757
KavyaAnjali-Rajendran <kavyaanjalirajendran@gmail.com>
2024-06-17 22:42:52 +0530

    best time to buy and sell stock

---
+++ best-time -to-sell-stock.js
@@ -0,0 +1,17 @@
+var maxProfit = function(prices) {
+  let minprice = prices[0];
+  let profit = 0;
+  for(let i = 0; i<=prices.length; i++)
+  {
+      if(prices[i]<minprice)
+      {
+          minprice = prices[i];
+      }
+      else if(prices[i]-minprice > profit)
+      {
+          profit = prices[i] - minprice;
+      }
+  }
+  return profit;
+  
+};
