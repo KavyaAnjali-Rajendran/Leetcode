a1e299104606bc8feaa68d869289e3575f55d437
KavyaAnjali-Rajendran <kavyaanjalirajendran@gmail.com>
2024-06-27 22:36:38 +0530

    Remove element

---
+++ Remove-element.js
@@ -0,0 +1,10 @@
+var removeElement = function(nums, val) {
+  let newIndex = 0;
+  for(let i = 0;i<nums.length;i++){
+     if(nums[i] != val ){
+      nums[newIndex]=nums[i];
+      newIndex++;
+     }
+  }
+   return newIndex
+};
